17a5ff65b1b38fc16d0a7a8763975080ba7f1644
datboiluiskrrt <datboiluiskrrt@gmail.com>
2022-08-12 23:28:05 -0500

    How to solve two sum using python

---
+++ index.py
@@ -0,0 +1,11 @@
+# Two sum
+def twoNumberSum(array, targetSum){
+  storage = set(num for num in array)
+
+  for num in array:
+    target = targetSum - num
+    if target in storage and target is not num:
+      return [num, target]
+
+  return []
+}
