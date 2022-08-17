0e873d798261021ff3df044263f0204a023fff5f
datboiluiskrrt <datboiluiskrrt@gmail.com>
2022-08-16 21:21:22 -0500

    217 Contains Duplicate Leetcode

--- index.py
+++ index.py
@@ -9,3 +9,15 @@ def twoNumberSum(array, targetSum){
 
   return []
 }
+# 217. Contains Duplicate 
+#Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+    
+        for value in nums:
+            if value in hashset:
+                return True
+            hashset.add(value)
+        return False
+    
