761eea8d059178e155dc52bd1f123cd20677e187
datboiluiskrrt <datboiluiskrrt@gmail.com>
2022-09-05 12:32:01 -0500

    347 Medium  top K frequent elements

--- index.js
+++ index.js
@@ -108,7 +108,46 @@ let isAnagram = function (s, t) {
   return true;
 };
 
+//347. Top K Frequent Elements
+/**
+ * Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+ */
+
+var topKFrequent = function (nums, k) {
+  const seen = {};
+
+  for (let num of nums) {
+    if (seen[num] === undefined) {
+      seen[num] = 1;
+    } else {
+      seen[num]++;
+    }
+  }
+
+  const bucket = [];
+  for (let i = 0; i <= nums.length; i++) {
+    bucket.push([]);
+  }
+
+  for (let key in seen) {
+    let count = seen[key];
+    bucket[count].push(key);
+  }
+
+  let res = [];
+  for (let i = bucket.length - 1; i >= 0; i--) {
+    if (bucket[i].length === 0) {
+      continue;
+    } else {
+      res = [...res, ...bucket[i]];
+    }
+  }
+
+  return res.slice(0, k);
+};
+
 console.log(twoSum([2, 7, 11, 15], 9));
 console.log(twoSum2([2, 7, 11, 15], 9));
 console.log(containsDuplicate([2, 7, 11, 15]));
 console.log(isAnagram("anagram", "nagaram"));
+console.log(topKFrequent([1, 1, 1, 2, 2, 3], (k = 2)));
